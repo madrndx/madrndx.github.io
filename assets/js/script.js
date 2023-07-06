ddc2c3145af054c726896a809fe75f4a3eb10f45
madrndx <rndx.ra@gmail.com>
2023-07-06 18:45:05 +0800

    asw

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,6 +173,26 @@ AOS.init();
         }
       })
     }
+   
+
+  /**
+   * Back to top button
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
     
 // Preloader
 
