4618d11d6aee19c844eecdb1069ab5115b55ea6c
rendi <rendi.abdul@germs.sg>
2023-11-17 13:57:38 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,37 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+
+
+
+
+var move = document.querySelector('.move');
+move.addEventListener('mousedown',mousedown);
+
+
+function mousedown(){
+
+    move.addEventListener('mousemove',mousemove);
+    move.addEventListener('mouseup',mouseup);
+    function mousemove(e){
+        var x = e.clientX -100 + 'px';
+        var y = e.clientY -100 + 'px';
+        this.style.left=x;
+        this.style.top=y;
+        this.style.background='yellow';
+        this.style.position='fixed';
+    }
+
+    function mouseup(){
+        move.removeEventListener('mousemove',mousemove);
+        this.style.background='black';
+        this.style.position='';
+    }
+    
+}
+
+
 (function() {
 
   "use strict";
